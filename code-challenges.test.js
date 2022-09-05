0cbf4411ac5b0bd3ec713f6e30798e3161b026fe
CathrineC <cjsr.carreon@gmail.com>
2022-09-04 18:06:54 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -21,7 +21,52 @@ const secretCodeWord2 = "Gobbledygook"
 const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+describe("convertLetters", () => {
+    it("returns a coded message", () => {
+        expect(convertLetters(secretCodeWord1)).toEqual('L4ck4d41s1c4l')
+        expect(convertLetters(secretCodeWord2)).toEqual('G0bbl3dyg00k')
+        expect(convertLetters(secretCodeWord3)).toEqual('3cc3ntr1c')
+    })
+})
+            // FAIL  ./code-challenges.test.js
+            // convertLetters
+            //   ✕ returns a coded message (1 ms)
+            // ● convertLetters › returns a coded message
+
 // b) Create the function that makes the test pass.
+
+// PSEUDO CODE:
+// Create a function named convertLetters
+// Input: takes in a string
+// Output: returns a coded message. 
+// If a word contains the letters a, e, i, and o, lower case or upper case, these letters must be converted into numbers such as 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
+// the given values have a data type string, it needs to be converted into an array
+// once, it is an array, the elements can be iterated to check if they are equal to the letters that needs to be converted.
+// convert the array letters into the corresponding numbers using conditional statements
+// if the letters doesn't fall into what needs to be changes, return the same value
+// convert the array of letters into a string
+// Expected output is a coded message with data type of string
+
+const convertLetters = (string) => {
+    changeToArray = string.split("")
+    const changeVowel = changeToArray.map(value => {
+        if (value === 'a' || value === 'A') {
+            return '4';
+        } if (value === 'e' || value === 'E') {
+            return '3'
+        } if (value === 'i' || value === 'I') {
+            return '1'
+        } if (value === 'o' || value === 'O') {
+            return '0'
+        } else
+            return value
+      });
+    return changeVowel.join('')
+}
+        // PASS  ./code-challenges.test.js
+        // convertLetters
+        //   ✓ returns a coded message (2 ms)
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
@@ -34,7 +79,43 @@ const letterA = "a"
 const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
+describe("selectWords", () => {
+    it("returns an array of all the words containing that particular letter", () => {
+        expect(selectWords(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+        expect(selectWords(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+})
+            // FAIL  ./code-challenges.test.js
+            // selectWords
+            //   ✕ returns an array of all the words containing that particular letter (1 ms)
+            // ● selectWords › returns an array of all the words containing that particular letter
+
 // b) Create the function that makes the test pass.
+
+// PSEUDO CODE:
+// Create a function named selectWords
+// Input: takes in an array and a single a letter
+// Output: returns an array of all the words containing that particular letter 
+// Create a function with parameters array and a letter
+// Create an empty array that will take strings of selected words
+// Check the letters of each value whether each value includes parameter letter
+// The parameter letter has to have 1. letter and 2. letter to be in uppercase to check if there are any upper case letters in the value being checked
+// Once the value is determined to have the parameter letter, the value will be pushed to the empty array
+// Expected output is an array of selected words.
+
+const selectWords = (array, letter) => {
+    arrWord = []
+    const containLetter = array.map(element => {
+        if (element.includes(letter) || element.includes(letter.toUpperCase())) {
+            return true && arrWord.push(element)
+        }   
+    })
+    return arrWord
+}
+
+            // PASS  ./code-challenges.test.js
+            // selectWords
+            //   ✓ returns an array of all the words containing that particular letter (3 ms)
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
@@ -49,4 +130,60 @@ const hand3 = [5, 5, 5, 5, 4]
 const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
+describe("fullHouse", () => {
+    it("determines whether or not the array is a full house", () => {
+        expect(fullHouse(hand1)).toEqual(true)
+        expect(fullHouse(hand2)).toEqual(false)
+        expect(fullHouse(hand3)).toEqual(false)
+        expect(fullHouse(hand4)).toEqual(true)
+    })
+})
+
 // b) Create the function that makes the test pass.
+
+// PSUEDO CODE:
+// Create a function named fullHouse
+// Input: takes in an array of 5 numbers
+// Output: determines whether or not the array is a “full house”
+// check each value in the given array
+// determine if there are 3 values of the same number and another 2 values of the same number by counting the instances of appearance of a card
+// return true if the set is complete - meaning its a full house
+// return false if it's not a full house
+
+class Card{
+    constructor(key){
+        this.key = key
+        this.count = 1        
+    }
+    addCard(){
+        this.count += 1
+    }
+}
+
+const fullHouse = (array) => {
+    sameCards = []
+    array.map(value => {
+        let found = false
+            found = sameCards.find(card => {
+                if (card.key === value){
+                    return true
+                }else {
+                    return false
+                }                
+            }) 
+            if (found){
+                found.addCard()
+            } else {
+                sameCards.push(new Card(value))  
+            }
+            return sameCards
+    })  
+    if ((sameCards[0].count === 3 && sameCards[1].count === 2) || (sameCards[0].count === 2 && sameCards[1].count === 3)) {
+        return true
+    } else {
+        return false
+    }     
+}  
+        // PASS  ./code-challenges.test.js
+        // fullHouse
+        //   ✓ determines whether or not the array is a full house (3 ms)
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,54 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a hash in Ruby?
 
-Your answer:
+Your answer: 
+I learned hash in Ruby in one of the lectures at the time that I attended a programming bootcamp. It is defined as a collection of keys and their values. An output of a hash is somewhat similar to an object in JavaScript, the difference is the look of the syntax. A hash uses symbol for the key and uses '=>' called hash rocket pointing to the value. Each pair is seperated by a comma and are inside curly braces{}. An example looks like below:
+{:shirt=>3, :shorts=>2, :socks=>"2 pairs"}
 
 Researched answer:
+I found the same answers online. Furthermore, I also found that a hash has a default value of nil. This means that if what is being selected inside a hash is not existing it will return an output of nil.
 
-2. What is a gem?
+2. What is a gem? 
 
-Your answer:
+Your answer: 
+I have encountered gems when I learned testing in Ruby. Gem is the software package that contains a library of Ruby code. It makes it easier to access custom methods. To install it, we simply type in gem install and the gem name. One of the gems that I used in Ruby is a Test Drived Development process which is the RSpec gem.
 
 Researched answer:
+I found the same answer online and also found out a way to find the list of gems available which is by simply typing in gem list into the terminal.
 
 3. What is Ruby on Rails?
 
 Your answer:
+I have been expose to working with Ruby on Rails. It is a MVC framework. MVC stands for Model View Controller. It will allow you to create models, view  and manipulate information. Ruby on rails is coded in Ruby which makes it as an open-source code that allows for programmers to create database. 
 
-Researched answer:
+Researched answer: From different sources online, I have read that Ruby on rails emphasizes coding by convention which means that developers can build framework swith flixibility and applies the principle of Don't Repeat Yourself which means it can use code multiple times.
 
 4. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: 
+I have used relational database when finding an association between tables using a primary key. The structure is made up of rows and columns. One of the columns holds the Primary Key. This is commonly used in Structued Query Language or SQL.
+So far, I have only been exposed to this kind of database.
 
-Researched answer:
+Researched answer: As I have read resources online, there are quite a number of other kinds of databases. They are Centralized Database, NoSQL Database, Distributed Database, Could Database, Netwrok Database, Object-Oriented Database, and Hierchical Database.
 
 5. What are primary keys? Why are they important?
 
 Your answer:
+In a relational database, primary keys are the ones that links the tables. It is important because it determines the association between tables. In rails, the primary is automatically assigned preventing any duplicates.
 
-Researched answer:
+Researched answer: As I have read from sources online, Primary key is a unique identifier of a table. It has to have unique values and cannot be empty or null or nil in Rails. Also, online it mentioned the importance of Primary key as something that will ensure low accessibility and it is the key used for queries, reading, modifying information.
+
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: A RESTful route stands for Representational State Transfer route. It provides mapping from HTTP verbs (get, post, put, delete, patch) to controller CRUD actions.
 
-2. JSON:
+2. JSON: JSON stands for JavaScript Object Notation. It is a text-based format similar to JavaScript format used to transmit data in web applications.
 
-3. ERB:
+3. ERB: ERB stands for Embedded Ruby. It is a Ruby language that is embedded to HTML. It allows to easily generate text in any quantity from templates.
 
-4. Params:
+4. Params: Params is short for parameter. It is a variable that is used inside a function.
 
-5. API:
+5. API: API stands for Application Programming Interface. It allows two applications to communicate to each other. This also allows to add security access.
